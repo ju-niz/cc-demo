fcb5cf03f149728e302b3d89540f6c88c84582a4
juniz <134158742+ju-niz@users.noreply.github.com>
2024-09-03 15:39:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 import os
+import socket
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
@@ -9,7 +10,8 @@ DEPLOYMENT_VERSION = os.environ.get('DEPLOYMENT_VERSION', 'unknown')
 
 @app.route('/')
 def index():
-    return render_template('index.html', version=DEPLOYMENT_VERSION)
+    ip_addr = socket.gethostbyname(socket.gethostname())
+    return render_template('index.html', version=DEPLOYMENT_VERSION, ip_addr=ip_addr)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
