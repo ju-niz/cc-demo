081461a79355d7cea0add69784cd48062fcafce1
juniz <134158742+ju-niz@users.noreply.github.com>
2025-02-20 14:38:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,28 @@ import os
 import socket
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import sys
+import logging
+
 
 load_dotenv()
 app = Flask(__name__)
 DEPLOYMENT_VERSION = os.environ.get('DEPLOYMENT_VERSION', 'unknown')
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+app.logger.addHandler(console_handler)
+logger.setLevel(logging.DEBUG)
 
 @app.route('/')
 def index():
+    logger.info("Demo app invoked")
     ip_addr = socket.gethostbyname(socket.gethostname())
+    logger.warning("Warning Log demo")
     return render_template('index.html', version=DEPLOYMENT_VERSION, ip_addr=ip_addr)
 
 if __name__ == '__main__':
